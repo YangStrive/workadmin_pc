01ec8b0c920ad9375269b5f81ad8058cdb410be6
yangfan09 <yangfan09@huohua.cn>
2024-08-05 18:54:40 +0800

    mock

---
+++ src/components/customizedScheduling/mock.js
@@ -0,0 +1,59 @@
+{
+  "errno": "0",
+  "errmsg": "Success",
+  "data": [
+      {
+        "user_id": "33157",
+        "user_name": "赵丽娟",
+        "date_schedule": [
+          {
+              "date": 1722787200,
+              "schedule": [
+                  {
+                      "task_id": "39435",
+                      "schedule_id": "1829",
+                      "position_id": "0",
+                      "schedule_name": "A",
+                      "start_time": "09:00",
+                      "end_time": "10:00",
+                      "rest_start_time": "",
+                      "rest_end_time": "",
+                      "work_hours": 1,
+                      "position_name": ""
+                  },
+                  {
+                      "task_id": "39435",
+                      "schedule_id": "1834",
+                      "position_id": "0",
+                      "schedule_name": "C",
+                      "start_time": "11:00",
+                      "end_time": "16:00",
+                      "rest_start_time": "",
+                      "rest_end_time": "",
+                      "work_hours": 5,
+                      "position_name": ""
+                  }
+              ]
+          },
+          {
+              "date": 1722873600,
+              "schedule": [
+                  {
+                      "task_id": "39435",
+                      "schedule_id": "1829",
+                      "position_id": "0",
+                      "schedule_name": "A",
+                      "start_time": "09:00",
+                      "end_time": "10:00",
+                      "rest_start_time": "",
+                      "rest_end_time": "",
+                      "work_hours": 1,
+                      "position_name": ""
+                  }
+              ]
+          }
+      ]
+    }
+  ]
+}
+        
